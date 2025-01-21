1181938ee6efd7bb0e2cd45e6cd9f6bd4de670ec
phillikiku <phillipkiku577@gmail.com>
2025-01-21 13:13:59 +0300

    Add corrections to Set 1 Question 8

--- SET 1/set 1 qn 8.js
+++ SET 1/set 1 qn 8.js
@@ -1,22 +1,5 @@
 //Use a ternary operator to check
 //if a number is odd or even.
 
-// declaring how the value will be entered.
-
 let digit = prompt("Please Enter a Digit");
-
-//function to check the status of the value entered.
 console.log(digit % 2 === 0 ? "EVEN" : "ODD");
-
-/*
-function checkDigit() {
- 
-  if (digit % 2 == 0) {
-    console.log(digit, "Digit is Even");
-  } else {
-    console.log(digit, "Digit is Odd");
-  }
-}
-
-console.log(checkDigit(digit));
-*/
