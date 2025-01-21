026b4733869ce972fd3b77554d5bb6052b35162d
phillikiku <phillipkiku577@gmail.com>
2025-01-21 12:41:57 +0300

    Add corrections for Set 1 Question 3

--- SET 1/set 1 qn 3.js
+++ SET 1/set 1 qn 3.js
@@ -1,22 +1,15 @@
 //A program that checks two variables
 // have the same value and type.
 
-// declaring how variables will be input.
-
 // function to check status of the variables.
 
 function check(variable1, variable2) {
-  
- // let variable1 = prompt("Enter first variable");
-  //let variable2 = prompt("Enter second variable");
-
   if (variable1 === variable2) {
-    console.log("matching");
+    return "Matching";
   } else {
-    console.log("Not-Matching");
+    return "Not-Matching";
   }
 }
 check();
 console.log(check("mna", 453));
-//console.log(check("man", "man"));
-
+console.log(check("man", "man"));
